c01d7b6f3118382bd9d9f9ed2e244a88792634f3
Andreas Jung <info@zopyx.com>
2021-12-04 16:16:43 +0100

    added controlpanel

---
+++ src/zopyx/typesense/browser/controlpanel.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+
+import json
+
+from plone.app.registry.browser import controlpanel
+from plone.registry.interfaces import IRegistry
+from zopyx.typesense import _
+from zopyx.typesense.interfaces import ITypesenseSettings
+from zope.component import getUtility
+
+
+class TypesenseSettingsEditForm(controlpanel.RegistryEditForm):
+
+    schema = ITypesenseSettings
+    label = _(u'Typesense settings')
+    description = _(u'')
+
+    def updateFields(self):
+        super(TypesenseSettingsEditForm, self).updateFields()
+
+    def updateWidgets(self):
+        super(TypesenseSettingsEditForm, self).updateWidgets()
+
+
+class TypesenseSettingsControlPanel(controlpanel.ControlPanelFormWrapper):
+    form = TypesenseSettingsEditForm
+
+    @property
+    def settings(self):
+        """ Returns setting as dict """
+        registry = getUtility(IRegistry)
+        settings = registry.forInterface(ITypesenseSettings)
+        result = dict()
+        for name in settings.__schema__:
+            result[name] = getattr(settings, name)
+        return result
+
+    def settings_json(self):
+        """ Returns setting as JSON """
+        return json.dumps(self.settings)
--- src/zopyx/typesense/interfaces.py
+++ src/zopyx/typesense/interfaces.py
@@ -1,8 +1,33 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
 
+from zope.interface import Interface
+from zope import schema
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
+from zopyx.typesense import _
 
-class IZopyxTypesenseLayer(IDefaultBrowserLayer):
+
+class IBrowserLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
+
+class ITypesenseSettings(Interface):
+    """ Connector settings """
+
+    pass
+    api_key = schema.TextLine(title=_('Typesense API key'),
+                                    default=u'',
+                                    required=False)
+
+    node1_url = schema.TextLine(title=_('URL of Typesense node 1'),
+                                         description=_(u'URL node 1'),
+                                         default="http://localhost:8101",
+                                         required=True)
+
+    node2_url = schema.TextLine(title=_('URL of Typesense node 2'),
+                                         description=_(u'URL node 2'),
+                                         required=False)
+
+    node3_url = schema.TextLine(title=_('URL of Typesense node 3'),
+                                         description=_(u'URL node 3'),
+                                         required=False)
