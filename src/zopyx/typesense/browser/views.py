f86b5db27d68c8fc7282ebcb7fdb3fce14c224ac
Andreas Jung <info@zopyx.com>
2021-12-05 15:57:52 +0100

    updated

---
+++ src/zopyx/typesense/browser/views.py
@@ -0,0 +1,77 @@
+from Products.Five.browser import BrowserView
+
+import typesense
+import plone.api
+
+import furl
+
+from zopyx.typesense.interfaces import ITypesenseSettings
+
+class View(BrowserView):
+
+
+    def get_typesense_client(self):
+        api_key = plone.api.portal.get_registry_record("api_key", ITypesenseSettings)
+        node1_url = plone.api.portal.get_registry_record("node1_url", ITypesenseSettings)
+        node2_url = plone.api.portal.get_registry_record("node2_url", ITypesenseSettings)
+        node3_url = plone.api.portal.get_registry_record("node3_url", ITypesenseSettings)
+
+        nodes = list()
+        for url in (node1_url, node2_url, node3_url):
+            if not url:
+                continue
+
+            f = furl.furl(url)
+            nodes.append(dict(
+                    host=f.host,
+                    port=f.port,
+                    protocol=f.scheme
+                    ))
+
+
+        client = typesense.Client({
+            'api_key': api_key,
+            'nodes': nodes,
+            'connection_timeout_seconds': 10
+        })
+        return client
+
+    def recreate_collection(self):
+
+        collection = plone.api.portal.get_registry_record("collection", ITypesenseSettings)
+        client = self.get_typesense_client()
+
+        try:
+            client.collections[collection].delete()
+            print(f"Deleted Typesense collection {collection}")
+        except Exception as e:
+            print(f"Could not delete Typesense collection {collection}")
+
+
+        create_response = client.collections.create({
+            "name": "typesense",
+            "fields": [
+                {"name": "path", "type": "string"},
+                {"name": "id", "type": "string"},
+                {"name": "title", "type": "string"},
+                {"name": "description", "type": "string"},
+                {"name": "text", "type": "string"},
+                {"name": "language", "type": "string", "facet": True},
+                {"name": "portal_type", "type": "string", "facet": True},
+                {"name": "review_state", "type": "string", "facet": False},
+            ],
+            "attributesToSnippet": [
+                'title',
+                'description',
+               'text:20',
+                ],
+            "attributesToHighlight": [
+                'title',
+                'description',
+               'text:20',
+                ]
+        })
+        print(f"Created Typesense collection {collection}")
+        print(create_response)
+
+        self.request.response.setStatus(201)
--- src/zopyx/typesense/indexer.py
+++ src/zopyx/typesense/indexer.py
@@ -7,85 +7,11 @@ from plone.restapi.services import Service
 from zope.component import queryMultiAdapter
 from zopyx.typesense.interfaces import ITypesenseSettings
 
+from .browser.views import View
+
 def index_content(context, event):
 
-    try:
-        collection = plone.api.portal.get_registry_record("collection", ITypesenseSettings)
-        api_key = plone.api.portal.get_registry_record("api_key", ITypesenseSettings)
-        node1_url = plone.api.portal.get_registry_record("node1_url", ITypesenseSettings)
-        node2_url = plone.api.portal.get_registry_record("node2_url", ITypesenseSettings)
-        node3_url = plone.api.portal.get_registry_record("node3_url", ITypesenseSettings)
-    except:
-        return
-
-    serializer = queryMultiAdapter((event.object, event.object.REQUEST), ISerializeToJson)
-    if not serializer:
-        return
-    try:
-        result = serializer()
-    except:
-        return
-
-    pprint.pprint(result)
-
-    nodes = list()
-    for url in (node1_url, node2_url, node3_url):
-        if not url:
-            continue
-
-        f = furl.furl(url)
-        nodes.append(dict(
-                host=f.host,
-                port=f.port,
-                protocol=f.scheme
-                ))
-
-
-    client = typesense.Client({
-    'api_key': api_key,
-    'nodes': nodes,
-    'connection_timeout_seconds': 2
-    })
-
-
-    try:
-        client.collections[collection].delete()
-    except Exception as e:
-        pass
-
-    try:
-        create_response = client.collections.create({
-            "name": "typesense",
-            "fields": [
-                {"name": "path", "type": "string"},
-                {"name": "id", "type": "string"},
-                {"name": "title", "type": "string"},
-                {"name": "description", "type": "string"},
-                {"name": "text", "type": "string"},
-                {"name": "language", "type": "string", "facet": True},
-                {"name": "portal_type", "type": "string", "facet": True},
-                {"name": "review_state", "type": "string", "facet": False},
-            ],
-            "attributesToSnippet": [
-                'title',
-                'description',
-               'text:20',
-                ],
-            "attributesToHighlight": [
-                'title',
-                'description',
-               'text:20',
-                ]
-        })
-        print(create_response)
-    except:
-        pass
-
-
-
-
-    retrieve_response = client.collections[collection].retrieve()
-    print(retrieve_response)
+    client = View(event.object, event.object.REQUEST).get_typesense_client()
 
     try:
         review_state = plone.api.content.get_state(context)
@@ -102,6 +28,8 @@ def index_content(context, event):
     d['review_state'] = review_state
     d['path'] = '/'.join(context.getPhysicalPath())
     pprint.pprint(d)
+
+    collection = plone.api.portal.get_registry_record("collection", ITypesenseSettings)
 
     response = client.collections[collection].documents.create(d)
     print(response)
