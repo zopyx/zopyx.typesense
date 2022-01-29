c02d334e7dbefb8fea4144027ee6c8b6aa00e3ad
Andreas Jung <info@zopyx.com>
2022-01-29 15:19:45 +0100

    updated

--- src/zopyx/typesense/cmd.py
+++ src/zopyx/typesense/cmd.py
@@ -6,11 +6,18 @@ import pprint
 app = typer.Typer()
 
 
+DEFAULT_SCHEMA = {
+  "name": "companies",
+  "fields": [
+    {"name": ".*", "type": "auto" }
+  ]
+}
+
+
 @app.command()
-def collections(
+def list(
         url=typer.Option(...),
         api_key=typer.Option(...)):
-    typer.echo("Collections...")
 
     f = furl.furl(url)
     nodes = [dict(host=f.host, port=f.port, protocol=f.scheme)]
@@ -19,6 +26,31 @@ def collections(
     for row in result:
         print(f"{row['name']:20s} {row['num_documents']}")
 
+@app.command()
+def create(
+        collection: str,
+        url=typer.Option(...),
+        api_key=typer.Option(...)):
+
+    f = furl.furl(url)
+    nodes = [dict(host=f.host, port=f.port, protocol=f.scheme)]
+    client = typesense.Client(dict(api_key=api_key, nodes=nodes))
+
+    schema = DEFAULT_SCHEMA.copy()
+    schema["name"] = collection
+    client.collections.create(schema)
+
+@app.command()
+def drop(
+        collection: str,
+        url=typer.Option(...),
+        api_key=typer.Option(...)):
+
+    f = furl.furl(url)
+    nodes = [dict(host=f.host, port=f.port, protocol=f.scheme)]
+    client = typesense.Client(dict(api_key=api_key, nodes=nodes))
+
+    client.collections[collection].delete()
 
 @app.command()
 def delete(username: str):
