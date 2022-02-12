e73bd3c3fa4f160323e7cff2187cbdf54a7733dc
Andreas Jung <info@zopyx.com>
2022-02-12 16:21:48 +0100

    removed

--- src/zopyx/typesense/browser/my-app/src/global.d.ts
+++
@@ -1,9 +0,0 @@
-import algoliasearch from 'algoliasearch/lite';
-import instantsearch from 'instantsearch.js/dist/instantsearch.production.min';
-
-declare global {
-  interface Window {
-    algoliasearch: typeof algoliasearch;
-    instantsearch: typeof instantsearch;
-  }
-}
