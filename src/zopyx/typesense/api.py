e5271b43b4d416df854a564eddfc405fc6feb89d
Andreas Jung <info@zopyx.com>
2022-10-17 15:24:49 +0200

    fix

--- src/zopyx/typesense/api.py
+++ src/zopyx/typesense/api.py
@@ -132,7 +132,7 @@ class API:
         if use_searchabletext:
             # use Plone's SearchableText implemenation
             indexable_text = SearchableText(obj)
-            indexable_headline = ""
+            indexable_headlines = ""
         else:
             # or our own indexable text content
             indexable_text = []
